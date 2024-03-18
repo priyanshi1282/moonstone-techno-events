50b1c6f2870b2c751001c0235574b9821f2ce9f1
Priyanshi Agrawal <priyanshiagrawal1282@gmail.com>
2024-03-19 02:10:19 +0530

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import EventSection from './components/EventSection'
 import MoonstoneSection from './components/MoonstoneSection'
 import Credits from './components/Credits'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <EventSection />
     <MoonstoneSection />
     <Credits />
+    <Footer />
    </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-gray-900/70 p-4 text-center'>
+        <p className='text-xl mb-3'>Developed with 	&#9829; by Team Medicaps</p>
+        <p className='text-sm font-sans'>Copyright reserved 2024</p>
+    </div>
+  )
+}
+
+export default Footer

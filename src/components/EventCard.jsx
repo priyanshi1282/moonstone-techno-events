86bd36875dbb43344839f5c8410a7fec97d7691d
Priyanshi Agrawal <priyanshiagrawal1282@gmail.com>
2024-03-20 00:59:24 +0530

    modal added

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,13 +1,59 @@
 import React from 'react'
-const EventCard = () => {
+import Modal from 'react-modal';
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    backgroundColor : 'black',
+    padding : '10px',
+    color : 'white'
+  },
+};
+
+const EventCard = (props) => {
+
+  let subtitle;
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = '#000000';
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
-    <div className='rounded-xl w-[200px] md:w-[250px] event-card'>
-        <img src="/assets/images/eventsPost/tic-tac-toe.jpeg" alt="event-img" className='w-full rounded-xl' />
+    <div className='rounded-xl w-[200px] md:w-[250px] event-card h-fit'>
+        <img src={props.item.img} alt="event-img" className='w-full rounded-xl' />
         <div className='px-2 md:py-4 py-2 text-center'>
-        <p className='md:text-xl text-lg font-semibold my-0 leading-none'>Kon banega cr</p>
-        <p className='mt-1 md:mb-3 mb-2'>23 march | 9pm</p>
-        <button className='bg-white hover:scale-95	 transition-all duration-300 ease-in-out rounded-full text-black px-6 py-1'>View Location</button>
+        <p className='md:text-xl text-lg font-semibold my-0 leading-none'>{props.item.title}</p>
+        <p className='mt-1 md:mb-3 mb-2'>{props.item.date} | {props.item.time}</p>
+        <button className='bg-white hover:scale-95 transition-all duration-300 ease-in-out rounded-full text-black px-6 py-1' onClick={openModal}>View Location</button>
         </div>
+
+        <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <button onClick={closeModal} className='absolute top-1 right-1 md:right-3 font-sans'>X</button>
+        <div className='max-w-[700px] m-2 md:m-4'>
+          <img src={props.item.venue} alt="" className='w-full' />
+        </div>
+      </Modal>
     </div>
   )
 }
--- src/components/EventSection.jsx
+++ src/components/EventSection.jsx
@@ -9,13 +9,14 @@ const EventSection = () => {
   return (
     <div className='m-4 md:mt-24 mt-20'>
         <Heading title= "Techno Events" />
-        <div className='md:mt-10 mt-5 flex  gap-8 md:gap-16 flex-wrap justify-center'>
+        <div className='md:mt-10 mt-5 flex  gap-8 md:gap-16 flex-wrap justify-center items-end'>
         {
             data.map((item)=>(
-                <EventCard key={item.id} />
+                <EventCard key={item.id} item = {item} />
             ))
         }
-             </div>
+        </div>
+        <img src="https://ik.imagekit.io/0ob92jg8l/Location/V.png?updatedAt=1710861107711" className='w-[80%] my-16 mx-auto rounded-xl' alt="" />
     </div>
   )
 }
